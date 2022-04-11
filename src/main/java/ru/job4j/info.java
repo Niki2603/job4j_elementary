993a22aba884ebbed7d8870f3819e129f9ac524c
Nikita <Soulmusik@yandex.ru>
2022-04-11 18:48:11 +0300

    4.2. Заголовок в файле java [#159036 #287578]

--- src/main/java/ru/job4j/info.java
+++ src/main/java/ru/job4j/info.java
@@ -1,8 +1,9 @@
 package ru.job4j;
 
 public class info {
-    public static void main(String[] args){
-         System.out.println("11.04.1993");
+    public static void main (String[] args){
+        System.out.println("11.04.2022");
+
     }
 }
 
