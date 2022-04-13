93a781461288a32f865a748396d8dc4a5ac638c5
Nikita <Soulmusik@yandex.ru>
2022-04-13 15:37:34 +0300

    4.1. Разбор первой программы. [#159033 #287548]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Nikita Petrakov");
         System.out.println("26.03.1993");
     }
