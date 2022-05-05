b450f6dd6303945071fc847dd1c3a3a91ef692ca
Nikita <Soulmusik@yandex.ru>
2022-05-05 15:57:53 +0300

    1.1. Что такое import [#504773 #297263]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double diffAndDivision(double first, double second) {
+        return diff(first, second)
+                + division(first, second);
+    }
+
+    public static double general(double first, double second) {
+        return sumAndMultiply(first, second)
+                + diffAndDivision(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + diffAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + general(10, 20));
+
+    }
+}
+
--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -1,12 +1,22 @@
 package ru.job4j.math;
 
 public class MathFunction {
-    public static double sumAndMultiply(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second);
+    public static double sum(double first, double second) {
+        return first + second;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+     public static double multiply(double first, double second) {
+         return first * second;
     }
+
+    public static double diff(double first, double second) {
+        return first + second;
+
+    }
+
+    public static double division(double first, double second) {
+        return first + second;
+    }
+
 }
+
