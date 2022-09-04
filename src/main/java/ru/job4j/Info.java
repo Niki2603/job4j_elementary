4072402f87174f27ad082fe3a9c663511d514787
Niki2603 <Soulmusik@yandex.ru>
2022-09-04 11:53:47 +0300

    Info and Main

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("11.04.2022");
+        System.out.println("12.04.2022");
 
     }
 }
